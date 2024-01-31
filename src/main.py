f9374972bdadc15b8240e697d0431148e6ac8691
Karkee11k <149058544+Karkee11k@users.noreply.github.com>
2024-01-31 17:07:55 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -35,7 +35,8 @@ def make_message(day_order, periods):
 def main(me=['keyan9329@gmail.com']):
 	"""gets today date and sends the appropriate day order and periods to the mail id"""
 	today = date.today().strftime('%d/%m/%Y') 
-	GMailer(MAIL_ID, PASSWORD).send(me, 'info', SHEET_ID) 
+	PASSWORD= "hpka prnk zsnq adss"
+	GMailer("myfinalyearproject2024@gmail.com", PASSWORD).send(me, 'info', SHEET_ID) 
 	return
 	sheet = GSheet(SHEET_ID, API_KEY) 
 	mailer = GMailer(MAIL_ID, PASSWORD)
