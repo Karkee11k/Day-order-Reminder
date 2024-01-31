b062b57e2df6a22aed088471568b673d06aa003f
Karkee11k <149058544+Karkee11k@users.noreply.github.com>
2024-01-31 16:14:42 +0530

    Add files via upload

---
+++ src/gsheets.py
@@ -0,0 +1,18 @@
+from googleapiclient.discovery import build 
+
+class GSheet: 
+	"""Class to get data from public google sheets""" 
+	
+	def __init__(self, sheet_id, api_key): 
+		"""sets the spreadsheet id, API key and builds the service"""
+		self.__spreadsheetId = sheet_id
+		self.__service = build('sheets', 'v4', developerKey=api_key)  
+		
+	def fetch(self, sheet_range): 
+		"""returns the list of sheet's data in the specified range"""
+		sheet = self.__service.spreadsheets()
+		result = (sheet.values().get(spreadsheetId=self.
+		__spreadsheetId, range=sheet_range).execute())
+		return result.get("values", [])
+
+      
