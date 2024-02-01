8d1af41b34c9986d76b5a78910fdc3018c1dd3c2
Karkee11k <149058544+Karkee11k@users.noreply.github.com>
2024-02-01 07:00:52 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -15,11 +15,11 @@ ME = os.getenv('ME')
 
 
 def get_day_order(date, calendar): 
-	"""Gives the day order, if date is a working day, else None"""
-	for row in calendar: 
-		if row[0] != date: 
-			continue 
-		return row[2] if 'y' in row[1] else None
+    """Gives the day order, if date is a working day, else None"""
+    for row in calendar: 
+	if row[0] != date: 
+	    continue 
+	return row[2] if 'y' in row[1] else None
 		
 
 def make_message(day_order, periods): 
@@ -34,41 +34,41 @@ def make_message(day_order, periods):
 		
 		
 def main(me=[ME]):
-	"""gets today date and sends the appropriate day order and periods to the mail id"""
-	today = date.today().strftime('%d/%m/%Y')
-	sheet = GSheet(SHEET_ID, API_KEY) 
-	mailer = GMailer(MAIL_ID, PASSWORD)
-	calendar = sheet.fetch('AGAC Calendar') 
+    """gets today date and sends the appropriate day order and periods to the mail id"""
+    today = date.today().strftime('%d/%m/%Y')
+    sheet = GSheet(SHEET_ID, API_KEY) 
+    mailer = GMailer(MAIL_ID, PASSWORD)
+    calendar = sheet.fetch('AGAC Calendar') 
 	
-	if calendar == None or len(calendar) == 0:  
-		mailer.send(me, 'info', 'no data found') 
-		return  
+    if not calendar:  
+        mailer.send(me, 'info', 'no data found') 
+	return  
 	
-	day_order = get_day_order(today, calendar)		
-	if not day_order: 
-		mailer.send(me, 'info', 'might be a holiday')
-		return 
+    day_order = get_day_order(today, calendar)		
+    if not day_order: 
+	mailer.send(me, 'info', 'might be a holiday')
+	return 
 			
-	_range = f'Periods!A{day_order}:E{day_order}'
-	periods = sheet.fetch(_range)[0]
-	_range = 'Mail ID!A:A'
-	mail_id = sheet.fetch(_range) 
+    _range = f'Periods!A{day_order}:E{day_order}'
+    periods = sheet.fetch(_range)[0]
+    _range = 'Mail ID!A:A'
+    mail_id = sheet.fetch(_range) 
 		
-	if mail_id == periods or not len(periods)*len(mail_id):
-		 mailer.send(me, 'info', 'no data found') 
-		 return 
+    if not periods or not mail_id:
+        mailer.send(me, 'info', 'no data found') 
+	return 
     
-	recipient = [id[0] for id in mail_id]
-	subject = f'Today Day Order {today}'
-	message = make_message(day_order, periods)		
-	mailer.send(recipient, subject, message) 	    
+    recipient = [id[0] for id in mail_id]
+    subject = f'Today Day Order {today}'
+    message = make_message(day_order, periods)		
+    mailer.send(recipient, subject, message) 	    
 
 
 if __name__ == '__main__':
-	try: 
-		main() 
-	except Exception as e: 
-		print(e)
+    try: 
+	main() 
+    except Exception as e: 
+	print(e)
 	    
 	    	    
 	    	    	   	    
